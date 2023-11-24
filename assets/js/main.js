838e9cb7b76e4afcd68bbf470b1b7e375092d970
pg3328 <pg3328@rit.edu>
2023-11-24 01:25:27 -0500

    completed professional Certifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,44 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
   el.addEventListener("click", toggleSkills);
 });
+
+/*============================Qualification Tabs =====================*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
+    });
+    tabs.forEach((otherTab) => {
+      // Fix the variable name here
+      otherTab.classList.remove("qualification__active");
+    });
+    target.classList.add("qualification__active");
+    tab.classList.add("qualification__active");
+  });
+});
+
+/*====================================Services======================================*/
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
