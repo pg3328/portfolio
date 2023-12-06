215114b96bc21143f2c9daa5f772461946c4cf3d
pg3328 <pg3328@rit.edu>
2023-12-05 22:18:36 -0500

    Almost a final website making changes to the content.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,33 @@
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+const phrases = ["Software Engineer", "Data Engineer"];
+const element = document.getElementById("typewriter");
+let sleepTime = 100;
+let curPhraseIndex = 0;
+
+const writeLoop = async () => {
+  while (true) {
+    let curWord = phrases[curPhraseIndex];
+    for (let i = 0; i < curWord.length; i++) {
+      element.innerText = curWord.substring(0, i + 1);
+      await sleep(sleepTime);
+    }
+    await sleep(sleepTime * 10);
+    for (let i = curWord.length; i > 0; i--) {
+      element.innerText = curWord.substring(0, i - 1);
+      await sleep(sleepTime);
+    }
+    await sleep(sleepTime * 5);
+    if (curPhraseIndex === phrases.length - 1) {
+      curPhraseIndex = 0;
+    } else {
+      curPhraseIndex++;
+    }
+  }
+};
+writeLoop();
+
 /* Menu Show Y Hidden*/
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
